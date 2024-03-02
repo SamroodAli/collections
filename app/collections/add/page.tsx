0c1756b0c266f4184b5449bba4d2d38e3c8b74be
SamroodAli <samrood@peoplegrove.com>
2024-03-02 18:55:33 +0530

    feat: choose a tenant instead of typing name

--- app/collections/add/page.tsx
+++ app/collections/add/page.tsx
@@ -1,22 +1,49 @@
 "use client";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 import AppLink from "@/components/AppLink";
 import { useForm } from "react-hook-form";
 import BFab from "@/components/BFab";
 import { Close } from "@mui/icons-material";
+import Autocomplete from "@mui/material/Autocomplete";
 
-export default function Page() {
+interface InputSchema {
+  tenant?: {
+    id: number;
+    name: string;
+    label: string;
+  } | null;
+  date?: string;
+  amount?: number;
+}
+
+export default function PTenant() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
-  } = useForm({
+  } = useForm<InputSchema>({
     defaultValues: {
-      name: "",
+      tenant: undefined,
       date: new Date().toISOString().substring(0, 10),
       amount: undefined,
     },
   });
+
+  const tenants = ["Alice", "Bob", "Charlie", "David"].map((name, id) => ({
+    label: name,
+    id,
+    name,
+  })); // Example names array
 
   const onSubmit = console.log;
 
@@ -24,13 +51,26 @@ export default function Page() {
     <Box sx={{ maxWidth: 500, mx: "auto", mt: 4, p: 2 }}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Box width="100%" sx={{ gap: 3, display: "grid", minWidth: "100%" }}>
-          <TextField
-            {...register("name", { required: true })}
-            label="Name"
-            fullWidth
-            error={!!errors["name"]}
-            helperText={errors["name"] ? "Please enter a name" : ""}
-          />
+          <FormControl fullWidth>
+            <Autocomplete
+              disablePortal
+              options={tenants}
+              onChange={(_, e) => {
+                setValue("tenant", e);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Tenant"
+                  error={!!errors["tenant"]}
+                />
+              )}
+            />
+
+            {!!errors["tenant"] ? (
+              <FormHelperText error>Please select a tenant</FormHelperText>
+            ) : null}
+          </FormControl>
           <TextField
             type="date"
             {...register("date", { required: true })}
@@ -52,7 +92,7 @@ export default function Page() {
             label="Amount"
             fullWidth
             error={!!errors["amount"]}
-            helperText={errors["amount"] ? "Please enter a amount" : ""}
+            helperText={errors["amount"] ? "Please enter an amount" : ""}
             InputLabelProps={{ shrink: true }}
           />
           <Button type="submit" variant="contained" color="primary">
